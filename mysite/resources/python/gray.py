d52d48085641393d4d2a6084093ea9acdb424b0d
fua243 <finoue@mis.doshisha.ac.jp>
2014-01-08 21:13:49 +0900

    modified

--- mysite/resources/python/gray.py
+++ mysite/resources/python/gray.py
@@ -3,78 +3,78 @@ from PIL import ImageOps
 
 if __name__ == "__main__":
 
-    i1 = Image.open("lena_original_1")
+    i1 = Image.open("lena_original_1.jpg")
     o1 = ImageOps.grayscale(i1)
     o1.save("lena_gray_1.jpg")
 
-    i2 = Image.open("lena_original_2")
+    i2 = Image.open("lena_original_2.jpg")
     o2 = ImageOps.grayscale(i2)
     o2.save("lena_gray_2.jpg")
 
-    i3 = Image.open("lena_original_3")
+    i3 = Image.open("lena_original_3.jpg")
     o3 = ImageOps.grayscale(i3)
     o3.save("lena_gray_3.jpg")
 
-    i4 = Image.open("lena_original_4")
+    i4 = Image.open("lena_original_4.jpg")
     o4 = ImageOps.grayscale(i4)
     o4.save("lena_gray_4.jpg")
 
-    i5 = Image.open("lena_original_5")
+    i5 = Image.open("lena_original_5.jpg")
     o5 = ImageOps.grayscale(i5)
     o5.save("lena_gray_5.jpg")
 
-    i6 = Image.open("lena_original_6")
+    i6 = Image.open("lena_original_6.jpg")
     o6 = ImageOps.grayscale(i6)
     o6.save("lena_gray_6.jpg")
 
-    i7 = Image.open("lena_original_7")
+    i7 = Image.open("lena_original_7.jpg")
     o7 = ImageOps.grayscale(i7)
     o7.save("lena_gray_7.jpg")
 
-    i8 = Image.open("lena_original_8")
+    i8 = Image.open("lena_original_8.jpg")
     o8 = ImageOps.grayscale(i8)
     o8.save("lena_gray_8.jpg")
 
-    i9 = Image.open("lena_original_9")
+    i9 = Image.open("lena_original_9.jpg")
     o9 = ImageOps.grayscale(i9)
     o9.save("lena_gray_9.jpg")
 
-    i10 = Image.open("lena_original_10")
+    i10 = Image.open("lena_original_10.jpg")
     o10 = ImageOps.grayscale(i10)
     o10.save("lena_gray_10.jpg")
 
-    i20 = Image.open("lena_original_20")
+    i20 = Image.open("lena_original_20.jpg")
     o20 = ImageOps.grayscale(i20)
     o20.save("lena_gray_20.jpg")
 
-    i30 = Image.open("lena_original_30")
+    i30 = Image.open("lena_original_30.jpg")
     o30 = ImageOps.grayscale(i30)
     o30.save("lena_gray_30.jpg")
 
-    i40 = Image.open("lena_original_40")
+    i40 = Image.open("lena_original_40.jpg")
     o40 = ImageOps.grayscale(i40)
     o40.save("lena_gray_40.jpg")
 
-    i50 = Image.open("lena_original_50")
+    i50 = Image.open("lena_original_50.jpg")
     o50 = ImageOps.grayscale(i50)
     o50.save("lena_gray_50.jpg")
 
-    i60 = Image.open("lena_original_60")
+    i60 = Image.open("lena_original_60.jpg")
     o60 = ImageOps.grayscale(i60)
     o60.save("lena_gray_60.jpg")
 
-    i70 = Image.open("lena_original_70")
+    i70 = Image.open("lena_original_70.jpg")
     o70 = ImageOps.grayscale(i70)
     o70.save("lena_gray_70.jpg")
 
-    i80 = Image.open("lena_original_80")
+    i80 = Image.open("lena_original_80.jpg")
     o80 = ImageOps.grayscale(i80)
     o80.save("lena_gray_80.jpg")
 
-    i90 = Image.open("lena_original_90")
+    i90 = Image.open("lena_original_90.jpg")
     o90 = ImageOps.grayscale(i90)
     o90.save("lena_gray_90.jpg")
 
-    i100 = Image.open("lena_original_100")
+    i100 = Image.open("lena_original_100.jpg")
     o100 = ImageOps.grayscale(i100)
     o100.save("lena_gray_100.jpg")
